73fbb549c501ec4d6b379bec4d78c6fb46abb3dd
Alex Dzul <alexexc2@gmail.com>
2012-07-01 09:51:35 -0500

    update Video 9
    ACtualizacion al video num 9

--- demo/apps/ventas/urls.py
+++ demo/apps/ventas/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns,url
 
-
 urlpatterns = patterns('demo.apps.ventas.views',
-	url(r'^add/producto/$','add_product_view',name='vista_agregar_producto'),
+	url(r'^add/producto/$','add_product_view',name= "vista_agregar_producto"),
 )
