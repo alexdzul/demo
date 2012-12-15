3f76f3599bcc775d70c57b73c70c8ae2e25154fa
alex dzul <alexexc2@gmail.com>
2012-12-15 09:33:48 -0600

    Tutorial No. 16 Youtube
    Aquí aprendimos a manejar el Paginator del Core de Django.

--- demo/apps/home/views.py
+++ demo/apps/home/views.py
@@ -6,7 +6,8 @@ from django.core.mail import EmailMultiAlternatives # Enviamos HTML
 
 from django.contrib.auth import login,logout,authenticate
 from django.http import HttpResponseRedirect
-
+# Paginacion en Django
+from django.core.paginator import Paginator,EmptyPage,InvalidPage
 
 def index_view(request):
 	return render_to_response('home/index.html',context_instance=RequestContext(request))
@@ -16,9 +17,18 @@ def about_view(request):
 	ctx = {'msg':mensaje}
 	return render_to_response('home/about.html',ctx,context_instance=RequestContext(request))
 
-def productos_view(request):
-	prod = producto.objects.filter(status=True) # Select * from ventas_productos where status = True
-	ctx = {'productos':prod}
+def productos_view(request,pagina):
+	lista_prod = producto.objects.filter(status=True) # Select * from ventas_productos where status = True
+	paginator = Paginator(lista_prod,3) # Cuantos productos quieres por pagina? = 3
+	try:
+		page = int(pagina)
+	except:
+		page = 1
+	try:
+		productos = paginator.page(page)
+	except (EmptyPage,InvalidPage):
+		productos = paginator.page(paginator.num_pages)
+	ctx = {'productos':productos}
 	return render_to_response('home/productos.html',ctx,context_instance=RequestContext(request))
 
 def singleProduct_view(request,id_prod):
